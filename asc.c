4358c2a7efb4cd74d3f2d9d591fc1d8b4b5eff24
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2024-12-07 13:57:09 -0300

    change variable type

--- asc.c
+++ asc.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    // Apenas uma variável será usada
-    for (int ascii = 32; ascii <= 126; ascii++) {
+    // Apenas uma variável char será usada
+    unsigned char ascii;
+
+    for (ascii = 32; ascii <= 126; ascii++) {
         // Imprime o código ASCII em decimal e seu caractere correspondente
         printf("Codigo: %d -> Caractere: %c\n", ascii, ascii);
     }

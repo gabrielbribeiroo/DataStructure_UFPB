ff64fd4e51a8b219637bdc1fbaf81bfa5831f54e
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2024-12-15 10:13:12 -0300

    show people over eighteen years old

---
+++ files/file2.c
@@ -0,0 +1,37 @@
+/*
+Mostrar o nome das pessoas maiores de 18 anos.
+*/
+
+#include <stdio.h>
+
+void main() {
+    FILE *f; // Ponteiro do arquivo
+    char name[12]; // Nome
+    int age; // Idade
+    int count = 0; // Contador de pessoas acima de 18 anos
+
+    f = fopen("people.txt", "r"); // Tenta abrir o arquivo para escrita
+    
+    // Verificação da variável após pedido
+    if (f == NULL) {
+        printf("\n\tErro ao abrir arquivo.");
+        return;
+    }
+
+    printf("Pessoas com mais de 18 anos:\n");
+
+    while ((fscanf(f, "%s", name) == 1) && (fscanf(f, "%d", &age) == 1)) {
+        // Verifica a idade das pessoas registradas
+        if (age > 18) {
+            count++; // Incrementa o contador de maiores
+            printf("Maior de idade %d: %s (%d anos)\n", count, name, age); // Mostra o nome da pessoa adulta
+        }
+    }
+        
+    // Verifica se não há ninguém maior de idade
+    if (count == 0) {
+        printf("Nenhuma pessoa com mais de 18 anos encontrada.\n");
+    }
+
+    fclose(f); // Fecha o arquivo
+}

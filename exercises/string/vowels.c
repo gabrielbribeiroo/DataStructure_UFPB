e7580875ecce635fe7212531cac8e3f9fe7cffe8
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-02-07 12:18:58 -0300

    adjusting the code to allow spaces

--- exercises/string/vowels.c
+++ exercises/string/vowels.c
@@ -6,6 +6,8 @@ Implement a function that takes a string as a parameter and returns the number o
 #include <stdio.h>
 #include <ctype.h>
 
+#define MAX 100
+
 int count_vowels(const char *str) {
     int count = 0;
 
@@ -21,11 +23,11 @@ int count_vowels(const char *str) {
 }
 
 int main() {
-    const char *test_str;
+    char test_str[MAX];
     
     // test
     printf("Enter a string: ");
-    scanf("%s", test_str);
+    fgets(test_str, sizeof(test_str), stdin);
 
     printf("Number of vowels: %d\n", count_vowels(test_str));
     

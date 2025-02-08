6ecd56297b6ed2379bc9e54b8a0defc0fc73578f
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-02-08 15:49:34 -0300

    allocate memory for the new string

--- exercises/string/lower.c
+++ exercises/string/lower.c
@@ -4,23 +4,46 @@ Implement a function that takes a string as a parameter and changes the occurren
 
 // libraries
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX 101
 
-void lower(char *str) {
-    for (int i = 0; i < strlen(str); i++) {
-        if (str[i] >= 'A' && str[i] <= 'Z') {
-            str[i] = str[i] + 32;
+char *lower(const char *str1) {
+    char *str2 = malloc(strlen(str1) + 1); // allocate memory for the new string
+
+    if (str2 == NULL) {
+        return NULL; // return NULL if memory allocation fails
+    }
+
+    strcpy(str2, str1); // copy the string to the new string
+
+    for (int i = 0; i < strlen(str2); i++) {
+        if (str2[i] >= 'A' && str2[i] <= 'Z') {
+            str2[i] = str2[i] + 32;
         }
     }
+
+    return str2; // return the lowercase string
 } 
 
 int main() {
     char str[MAX];
+
+    // get the string from the user
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
-    lower(str);
-    printf("The string in lowercase is: %s", str);
+
+    // call the function to convert the string to lowercase
+    char *lower_str = lower(str); 
+    
+    if (lower_str != NULL) {
+        printf("The string in lowercase is: %s", lower(str));
+        free(lower_str); // free the allocated memory
+    } 
+    else {
+        printf("Memory allocation failed.\n");
+    }
+    
     return 0;
 }

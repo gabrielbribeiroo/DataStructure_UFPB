5de460cd7fb40c0b4c38814e9e1505fceb7918cd
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-02-08 15:56:34 -0300

    allocate memory for the new string

--- exercises/string/spin_string.c
+++ exercises/string/spin_string.c
@@ -4,43 +4,60 @@ For example, the string “home" would be changed to “ehom”.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX 101
 
-void spin_string(char *str) {
-    int len = strlen(str);
+char *spin_string(const char *str1) {
+    char *str2 = malloc(strlen(str1) + 1); // allocate memory for the new string
+
+    if (str2 == NULL) {
+        return NULL; // return NULL if memory allocation fails
+    }
+
+    strcpy(str2, str1); // copy the string to the new string
+
+    int len = strlen(str2); // get the length of the string
 
     // remove the newline character from the string
-    if (str[len - 1] == '\n') {
-        str[len - 1] = '\0';
+    if (str2[len - 1] == '\n') {
+        str2[len - 1] = '\0';
         len--; // update the length of the string
     }
 
     if (len <= 1) return; // if the string has 0 or 1 character, there is nothing to do
 
-    char last = str[len - 1]; // save the last character
+    char last = str2[len - 1]; // save the last character
 
     // shift the characters one position to the right
     for (int i = len - 1; i > 0; i--) {
-        str[i] = str[i - 1];
+        str2[i] = str2[i - 1];
     }
 
-    str[0] = last; // move the last character to the first position
+    str2[0] = last; // move the last character to the first position
 }
 
 int main() {
     char str[MAX];
 
+    // get the string from the user
     printf("Enter a string: ");
     if (fgets(str, sizeof(str), stdin) == NULL) {
         printf("Error reading the string\n");
         return 1;
     }
 
-    spin_string(str);
+    // call the function to convert the string
+    char *new_str = spin_string(str);
 
-    printf("The new string is: %s", str);
+    if (new_str != NULL) {
+        printf("The new string is: %s", new_str);
+        free(new_str); // free the allocated memory
+    } 
+    else {
+        printf("Memory allocation failed.\n");
+    }
 
     return 0;
 }

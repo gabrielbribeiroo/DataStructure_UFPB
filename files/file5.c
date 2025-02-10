ca75d114e27d870c463f2113ed2615bd9e2c3898
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-02-10 13:21:13 -0300

    joining files into one

---
+++ files/file5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define SIZE_NAME 50
+
+int main() {
+    FILE *f1, *f2, *f3, *db;
+    char name[SIZE_NAME], sex;
+    int age;
+
+    f1 = fopen("names.txt", "r");
+    if (f1 == NULL) {
+        printf("Error opening name file\n");
+        return 1;
+    }
+
+    f2 = fopen("ages.txt", "r");
+    if (f2 == NULL) {
+        printf("Error opening age file\n");
+        fclose(f1);
+        return 1;
+    }
+
+    f3 = fopen("sex.txt", "r");
+    if (f3 == NULL) {
+        printf("Error opening sex file\n");
+        fclose(f1);
+        fclose(f2);
+        return 1;
+    }
+
+    db = fopen("database.txt", "w");
+    if (db == NULL) {
+        printf("Error opening database file\n");
+        fclose(f1);
+        fclose(f2);
+        fclose(f3);
+        return 1;
+    }
+
+    while (fscanf(f1, "%s", name) != EOF) {
+        fscanf(f2, "%d", &age);
+        fscanf(f3, " %c", &sex);
+        fprintf(db, "%s %d %c\n", name, age, sex);
+    }
+
+    fclose(f1);
+    fclose(f2);
+    fclose(f3);
+    fclose(db);
+
+    printf("Data successfully merged into database.txt\n");
+
+    return 0;
+}

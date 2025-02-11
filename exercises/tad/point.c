f94c5ae1e36fd620829d9209e372a3cd13c05d94
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-02-11 20:57:12 -0300

    add new information to TAD point, such as adding and subtracting points.

---
+++ exercises/tad/point.c
@@ -0,0 +1,56 @@
+/*
+Add new information to TAD point, such as adding and subtracting points.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct Point {
+    float x;
+    float y;
+};
+
+typedef struct Point point;
+
+point *create_point(float x, float y) {
+    point* p = (point*) malloc(sizeof(point));
+    p->x = x;
+    p->y = y;
+    return p;
+}
+
+void free_point(point* p) {
+    free(p);
+}
+
+point* add_points(point* p1, point* p2) {
+    return create_point(p1->x + p2->x, p1->y + p2->y);
+}
+
+point* subtract_points(point* p1, point* p2) {
+    return create_point(p1->x - p2->x, p1->y - p2->y);
+}
+
+float distance(point* p1, point* p2) {
+    return sqrt(pow(p1->x - p2->x, 2) + pow(p1->y - p2->y, 2));
+}
+
+int main() {
+    point* p1 = create_point(1, 2);
+    point* p2 = create_point(3, 4);
+
+    point* p3 = add_points(p1, p2);
+    point* p4 = subtract_points(p1, p2);
+
+    printf("Distance between p1 and p2: %.2f\n", distance(p1, p2));
+    printf("Distance between p1 and p3: %.2f\n", distance(p1, p3));
+    printf("Distance between p1 and p4: %.2f\n", distance(p1, p4));
+
+    free_point(p1);
+    free_point(p2);
+    free_point(p3);
+    free_point(p4);
+
+    return 0;
+}

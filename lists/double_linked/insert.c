fd43f96575f9c2b4407329cf4d69e62a4c288e4f
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-02-24 11:43:24 -0300

    file for implementing list insertion functions

---
+++ lists/double_linked/insert.c
@@ -0,0 +1,10 @@
+/**
+ * @file insert.c
+ * @brief Implementation of the functions to insert elements in a linked list.
+ * 
+ * @author Gabriel Ribeiro
+ * @version 1.0
+ * @date 2025-02-24
+ */
+
+#include "insert.h" // include the header file with the function prototypes

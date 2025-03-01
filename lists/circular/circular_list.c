a08a5ddaac25125dc492956d55f96cb87120156e
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-01 18:33:49 -0300

    rename file

---
+++ lists/circular/circular_list.c
@@ -0,0 +1,14 @@
+/**
+ * @file circular_list.c
+ * @brief Implementation of circular linked list.
+ * 
+ * @author Gabriel Ribeiro
+ * @version 1.0
+ * @date 2025-03-01 
+ */
+
+// include libraries
+#include "create.h"
+#include "insert.h"
+#include "delete.h"
+#include "route.h"

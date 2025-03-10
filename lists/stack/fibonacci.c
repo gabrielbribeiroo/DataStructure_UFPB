b202f898eddbf9d9466a44b3be965b165f0032c2
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-10 10:59:23 -0300

    returns the nth fibonacci number, using recursion

---
+++ lists/stack/fibonacci.c
@@ -0,0 +1,32 @@
+/*
+Returns the nth fibonacci number, using recursion.
+*/
+
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else {
+        return fibonacci(n -1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    int n;
+
+    // Get the number of fibonacci numbers to generate
+    printf("Enter the number of fibonacci numbers to generate: ");
+    scanf("%d", &n);
+
+    // Print the fibonacci numbers
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    
+    return 0;
+}

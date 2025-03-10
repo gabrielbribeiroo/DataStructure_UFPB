df596c5026621bc9e830a55c422530bc4ba9c7b4
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-10 11:34:45 -0300

    remove global variable

--- lists/stack/insert.c
+++ lists/stack/insert.c
@@ -3,23 +3,22 @@
  * @brief This file implements the insertion functions for the stack.
  * 
  * @author Gabriel Ribeiro
- * @version 1.0
+ * @version 1.1
  * @date 2025-03-10
  */
 
 #include "insert.h" // include the header file with the function prototypes
 
-int data;
-
 // list by vector
 // inserts a new element at the end of the vector
 void push_vector(struct vector *l) {
-    printf("Data: ");
-    scanf("%d", &data);
     if (vector_full(*l)) { // if the list is full
         printf("The list is full.\n"); // prints a message
         return; // returns
     }
+    int data;
+    printf("Data: ");
+    scanf("%d", &data);
     l->data[l->ctr] = data; // inserts the data
     l->ctr++; // increments the size
 }
@@ -28,6 +27,7 @@ void push_vector(struct vector *l) {
 // list by linked list
 // inserts a new element at the beginning of the linked list
 void push_linked_list(struct node **l) {
+    int data;
     printf("Data: ");
     scanf("%d", &data);
     struct node *new_node; // creates a new node

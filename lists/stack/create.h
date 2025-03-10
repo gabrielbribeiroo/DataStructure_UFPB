b1220a0302a40d749bb41c94f02bf9d0006499d1
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-10 11:21:30 -0300

    header of stack creation functions by vector or linked list

---
+++ lists/stack/create.h
@@ -0,0 +1,74 @@
+#ifndef CREATE_H // checks if the header has already been included
+#define CREATE_H // if not, define the header
+
+#include <stdio.h> // for printf
+#include <stdlib.h> // for malloc
+
+#define SIZE 10 // size of the vector
+
+// list by vector
+/**
+ * @brief Defines the structure for a vector in a circular list.
+ * 
+ * @param data The data stored in the vector.
+ * @param ctr The control variable.
+ */
+struct vector {
+    int data[SIZE]; // data
+    int ctr; // control variable
+};
+
+/**
+ * @brief Initializes the list to be empty.
+ * 
+ * @param l A pointer to the head of the vector.
+ * @return void
+ */
+void create_vector_list(struct vector *l);
+
+/**
+ * @brief Checks if the list is full.
+ * 
+ * @param l The vector.
+ * @return char 1 if the list is full, 0 otherwise.
+ */
+char vector_full(struct vector l);
+
+/**
+ * @brief Checks if the list is empty.
+ * 
+ * @param l The vector.
+ * @return char 1 if the list is empty, 0 otherwise.
+ */
+char vector_empty(struct vector l);
+
+
+// list by linked list
+/**
+ * @brief Defines the structure for a node in the linked list.
+ * 
+ * @param data The data stored in the node.
+ * @param next A pointer to the next node.
+ */
+struct node {
+    int data;
+    struct node *next;
+};
+
+/**
+ * @brief Initializes the linked list to be empty.
+ * 
+ * @param l A double pointer to the head of the linked list.
+ * @return void
+ */
+void create_linked_list(struct node **l);
+
+/**
+ * @brief Checks if the linked list is empty.
+ * 
+ * @param l A pointer to the head of the linked list.
+ * @return char 1 if the list is empty, 0 otherwise.
+ */
+char linked_list_empty(struct node *l);
+
+#endif // end of the header guard

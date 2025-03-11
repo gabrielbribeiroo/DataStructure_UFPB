5dd5484f30f5662e4cec12e8f305424a782693ea
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-11 11:05:48 -0300

    create menu

---
+++ lists/stack/list.c
@@ -0,0 +1,91 @@
+/**
+ * @file list.c
+ * @brief Implementation of stack.
+ * 
+ * @author Gabriel Ribeiro
+ * @version 1.0
+ * @date 2025-03-10 
+ */
+
+#include "create.h" 
+#include "insert.h"
+#include "delete.h"
+#include "route.h"
+#include "menu.h"
+
+int main() {
+    int opt1, opt2;
+    menu();
+    scanf("%d", &opt1);
+
+    switch (opt1) {
+        case 1: {
+            struct vector_list *l; 
+            create_vector_list(&l);
+            display_menu_vector_list();
+            scanf("%d", &opt2);
+            switch (opt2) {
+                case 1:
+                    push_vector(&l);
+                    break;
+                case 2:
+                    pop_vector(&l);
+                    break;
+                case 3:
+                    search_vector_list(&l);
+                    break;
+                case 4:
+                    print_vector_list(&l);
+                    break;
+                case 5:
+                    clean_vector_list(&l);
+                    break;
+                case 6:
+                    exit(0);
+                    break;
+                default:
+                    printf("Invalid option\n");
+                    break;
+            }
+            break;
+        }
+        case 2: {
+            struct node **l;
+            create_linked_list(l);
+            display_menu_linked_list();
+            scanf("%d", &opt2);
+            switch (opt2) {
+                case 1:
+                    push_linked_list(l);
+                    break;
+                case 2:
+                    pop_linked_list(l);
+                    break;
+                case 3:
+                    search_linked_list(l);
+                    break;
+                case 4:
+                    print_linked_list(l);
+                    break;
+                case 5:
+                    clean_linked_list(&l);
+                    break;
+                case 6:
+                    exit(0);
+                    break;
+                default:
+                    printf("Invalid option\n");
+                    break;
+            }
+            break;
+        }
+        case 3:
+            exit(0);
+            break;
+        default:
+            printf("Invalid option\n");
+            break;
+    }
+
+    return 0;
+}
--- lists/stack/menu.h
+++ lists/stack/menu.h
@@ -1,36 +1,11 @@
-/**
- * @file menu.c
- * @brief Menu display function to interact with the user
- * 
- * @author Gabriel Ribeiro
- * @version 1.0
- * @date 2025-03-10
- */
+#ifndef MENU_H // checks if the header has already been included
+#define MENU_H // if not, define the header
 
-#include "create.h"
+#include "create.h" // for vector, and linked list
 
-void menu() {
-    printf("\n1. List with vectors");
-    printf("\n2. List with linked list");
-    printf("\n3. Exit");
-}
+// functions
+void menu();
+void display_menu_vector_list();
+void display_menu_linked_list();
 
-void display_menu_vector_list() {
-    printf("1. Insert at the end\n");
-    printf("2. Remove from the end\n");
-    printf("3. Consult\n");
-    printf("4. Print\n");
-    printf("5. Clear\n");
-    printf("6. Exit\n");
-    printf("Option: ");
-}
-
-void display_menu_linked_list() {
-    printf("\n1 - Insert start");
-    printf("\n2 - Delete start");
-    printf("\n3 - Consult");
-    printf("\n4 - Print");
-    printf("\n5 - Clear");
-    printf("\n6 - Exit");
-    printf("\nOption: ");
-}
+#endif // end of the header guard

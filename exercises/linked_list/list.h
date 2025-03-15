1338d90340b1ae8b2a55b52107207c947e152c4c
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-15 11:00:21 -0300

    documenting the functions

--- exercises/linked_list/list.h
+++ exercises/linked_list/list.h
@@ -4,21 +4,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Defines the structure for a node in the linked list.
+ * @param data The data stored in the node.
+ * @param next A pointer to the next node.
+ */
 struct list {
     int data;
     struct list *next;
 };
 
-// Create a new linked list
+/**
+ * @brief Initializes the linked list to be empty. 
+ * @param l A double pointer to the head of the linked list.
+ * @return void
+ */
 void create(struct list **l) {}
 
-// Insert a new node at the end of the list
+/**
+ * @brief Checks if the list is empty
+ * @param  A pointer to the head of the linked list.
+ * @return int Returns 1 if the list is empty, 0 otherwise. * 
+ */
+int is_empty(struct list *l) {}
+
+/**
+ * @brief Inserts a node at the end of the list
+ * @param l A pointer to the head of the linked list.
+ * @return void
+ */
 void insert_end(struct list *l) {}
 
-// Remove the last node from the list
+/**
+ * @brief Removes a node from the end of the list
+ * @param l A pointer to the head of the linked list.
+ * @return void
+ */
 void remove_end(struct list *l) {}
 
-// Display the list
+/**
+ * @brief Displays the list
+ * @param l A pointer to the head of the linked list.
+ * @return void
+ */
 void display(struct list *l) {}
 
 #endif // end of the header guard

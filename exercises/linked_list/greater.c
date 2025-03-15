d4634d1d9bae7f9cd93d5849fff5bff248c33388
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-15 10:56:08 -0300

    list manipulation functions header

---
+++ exercises/linked_list/greater.c
@@ -0,0 +1,6 @@
+/*
+Consider linked lists of integer values ​​and implement a function to return the number of nodes in the list that have the info field with values ​​greater than n. 
+This function must obey the prototype:
+int greater(List * l, int n);
+*/
+
---
+++ exercises/linked_list/list.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list {
+    int data;
+    struct list *next;
+};
+
+// Create a new linked list
+void create(struct list **l) {}
+
+// Insert a new node at the end of the list
+void insert_end(struct list *l) {}
+
+// Remove the last node from the list
+void remove_end(struct list *l) {}
+
+// Display the list
+void display(struct list *l) {}

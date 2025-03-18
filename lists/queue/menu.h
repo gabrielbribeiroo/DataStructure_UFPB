fb9b8e7eefc6066408122a1d6b605302dc17acf2
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-18 13:29:24 -0300

    add menu header file with function declarations for displaying various list menus

---
+++ lists/queue/menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H // checks if the header has already been included
+#define MENU_H // if not, define the header
+
+#include "create.h" // for vector, and linked list
+
+// functions
+void menu();
+void display_menu_vector_list();
+void display_menu_linked_list();
+void display_menu_double_linked_list();
+
+#endif // end of the header guard

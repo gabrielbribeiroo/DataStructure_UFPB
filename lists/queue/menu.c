71b833a8f29f697944e98f70f3a28ecdb2d66ef8
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-18 13:33:57 -0300

    add menu display functions for user interaction with queue options

---
+++ lists/queue/menu.c
@@ -0,0 +1,48 @@
+/**
+ * @file menu.c
+ * @brief Menu display function to interact with the user
+ * 
+ * @author Gabriel Ribeiro
+ * @version 1.0
+ * @date 2025-03-18
+ */
+
+#include "menu.h"
+
+void menu() {
+    printf("\n1. Queue with vectors");
+    printf("\n2. Queue with linked list");
+    printf("\n3. Queue with double linked list");
+    printf("\n4. Exit");
+    printf("\nOption: ");
+}
+
+void display_menu_vector_list() {
+    printf("\n1. Insert at the end");
+    printf("\n2. Remove from the begin");
+    printf("\n3. Consult");
+    printf("\n4. Print");
+    printf("\n5. Clear");
+    printf("\n6. Exit");
+    printf("\nOption: ");
+}
+
+void display_menu_linked_list() {
+    printf("\n1 - Insert at the end");
+    printf("\n2 - Remove from the begin");
+    printf("\n3 - Consult");
+    printf("\n4 - Print");
+    printf("\n5 - Clear");
+    printf("\n6 - Exit");
+    printf("\nOption: ");
+}
+
+void display_menu_double_linked_list() {
+    printf("\n1 - Insert at the end");
+    printf("\n2 - Remove from the begin");
+    printf("\n3 - Consult");
+    printf("\n4 - Print");
+    printf("\n5 - Clear");
+    printf("\n6 - Exit");
+    printf("\nOption: ");
+}

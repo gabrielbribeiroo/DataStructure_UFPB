16b8a887a74806403c0de37cdcb80c28cffbd167
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-17 21:35:31 -0300

    add header file for queue removal functions

---
+++ lists/queue/delete.h
@@ -0,0 +1,30 @@
+#ifndef DELETE_H // checks if the header has already been included
+#define DELETE_H // if not, define the header
+
+#include "create.h"
+
+// queue by vector
+/**
+ * @brief Removes the first element of the queue
+ * @param l pointer to the queue
+ * @return void
+ */
+void remove_vector(struct vector *l);
+
+// queue by linked list
+/**
+ * @brief Removes the first element of the queue
+ * @param l pointer to the queue
+ * @return void
+ */
+void remove_linked_list(struct queue *l);
+
+// queue by doubly linked list
+/**
+ * @brief Removes the first element of the queue
+ * @param l pointer to the queue
+ * @return void
+ */
+void remove_doubly_linked_list(struct dqueue *l);
+
+#endif // end of the header guard

56c354f49090584008333fc394fa31eeee1151c7
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-03-22 20:57:21 -0300

    add heterogeneous list creation: define header for node structures and creation functions for types A, B, and C

---
+++ lists/heterogeneous/create.h
@@ -0,0 +1,17 @@
+#ifndef CREATE_H // checks if the header has already been included
+#define CREATE_H // if not, define the header
+
+#include <stdio.h> // for printf
+#include <stdlib.h> // for malloc
+
+// structures
+struct node;
+struct a;
+struct b;
+struct c;
+
+void create_a(struct a**);
+void create_b(struct b**);
+void create_c(struct c**);
+
+#endif // end of the header guard

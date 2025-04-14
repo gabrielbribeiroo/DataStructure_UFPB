a70bf3c4323d3830389f396eb9cb1c4ca85caea1
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-04-14 19:42:42 -0300

    implement create_type1 and create_type2 functions for graph data structures

--- graph/graph.c
+++ graph/graph.c
@@ -18,6 +18,29 @@ struct type2 {
 };
 typedef struct type2 Type2;
 
+Type1 *create_type1(int value) {
+    Type1 *new_type1 = (Type1 *)malloc(sizeof(Type1));
+    if (new_type1 == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new_type1->data = value;
+    new_type1->t2 = NULL;
+    new_type1->t1 = NULL;
+    return new_type1;
+}
+
+Type2 *create_type2(int value) {
+    Type2 *new_type2 = (Type2 *)malloc(sizeof(Type2));
+    if (new_type2 == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new_type2->data = value;
+    new_type2->next = NULL;
+    return new_type2;
+}
+
 int main() {
     return 0;
 }
